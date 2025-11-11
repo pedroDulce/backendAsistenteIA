5e7dbb606177cb85d9a6336f46cadd3570061bcd
pedroDulce <pedro.dulce@gmail.com>
2025-11-11 21:43:46 +0100

    s

--- src/main/java/com/example/qaassistant/controller/RankingDTO.java
+++ src/main/java/com/example/qaassistant/controller/RankingDTO.java
@@ -1,6 +1,28 @@
 package com.example.qaassistant.controller;
 
 import com.example.qaassistant.model.Aplicacion;
+import com.example.qaassistant.model.EstadoAplicacion;
+import lombok.Data;
 
-public record RankingDTO(Aplicacion aplicacion, Float cobertura) {
+import java.time.LocalDateTime;
+
+@Data
+public class RankingDTO {
+
+    private String nombre;
+    private String descripcion;
+    private String equipoResponsable;
+    private EstadoAplicacion estado;
+    private LocalDateTime fechaCreacion;
+    private Float cobertura;
+
+    public RankingDTO(Aplicacion aplicacion, Float cobertura) {
+        this.nombre = aplicacion.getNombre();
+        this.descripcion = aplicacion.getDescripcion();
+        this.equipoResponsable = aplicacion.getEquipoResponsable();
+        this.estado = aplicacion.getEstado();
+        this.fechaCreacion = aplicacion.getFechaCreacion();
+        this.cobertura = cobertura;
+    }
+
 }
